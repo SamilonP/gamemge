7da985c62234da85a206e50ec4c7c1ddab2da34c
EmrickF <erik.finnberg07@gmail.com>
2025-03-04 09:06:08 +0200

    pull

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import Phaser from 'phaser'
 import './style.css'
+import Gamescene from './Gamescene'
 
 
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 320 * 1.5,
     height: 480 * 1.5,
-    parent: 'app',
+    
     scene: [
         
     ],
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { x: 0, y: 900 },
             debug: false
         }
     }
 }
     
 // @ts-ignore
-const game = new Game(config)
+const game = new Gamescene(config)
