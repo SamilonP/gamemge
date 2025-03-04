b389e7254baa0dabbfa13f48ed5837fd3ddf288c
groenbacke <frojdoadam@gmail.com>
2025-03-04 13:55:02 +0200

    added movement

--- src/rectangle.ts
+++ src/rectangle.ts
@@ -1,20 +1,35 @@
+let player: any;
+let enemy: any;
+
 export default class Rectangle {
-   private scene: Phaser.Scene;
+    private scene: Phaser.Scene;
 
-   constructor(scene: Phaser.Scene) {
-       this.scene = scene;
-   }
+    constructor(scene: Phaser.Scene) {
+        this.scene = scene;
+    }
+ 
+    preload() {
+        
+    }
+ 
+    create() {
+        const X = this.scene.cameras.main.width / 2;
+        const Y = this.scene.cameras.main.height / 2;
+    
+        const rectWidth = 100;
+        const gap = 200;
+    
+        //  two rectangles
+        player = this.scene.physics.add.sprite(X - rectWidth / 2 - gap / 2, Y, "flarg").setScale(.2)
+        enemy = this.scene.physics.add.sprite(X + rectWidth / 2 + gap / 2, Y, "flarg").setScale(.2)
 
-   create() {
-       const X = this.scene.cameras.main.width / 2;
-       const Y = this.scene.cameras.main.height / 2;
-
-       const rectWidth = 100;
-       const rectHeight = 100;
-       const gap = 200;
-
-       //  two rectangles
-       this.scene.add.rectangle(X - rectWidth / 2 - gap / 2, Y, rectWidth, rectHeight, 0x808080);
-       this.scene.add.rectangle(X + rectWidth / 2 + gap / 2, Y, rectWidth, rectHeight, 0x808080);
-   }
+        player.body.setImmovable(true)
+        player.body.setAllowGravity(false)
+        enemy.body.setImmovable(true)
+        enemy.body.setAllowGravity(false)
+    }
+ 
+    move(direction: number) {
+        player.body.x += direction
+    }
 }
